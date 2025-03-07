4f8826f737986536e1ec744a71217e5b840dd7f1
SRF0x41 <sergio.rodriguezflores26@gmail.com>
2025-03-07 01:15:03 -0700

    updated the main

--- main.cpp
+++ main.cpp
@@ -32,26 +32,3 @@ int main()
 
     return 0;
 }
-
-void usageExample()
-{
-    // Set device (use CUDA if available, otherwise CPU)
-    // Possibly move this to the AbsModel constructor
-    torch::Device device(torch::cuda::is_available() ? torch::kCUDA : torch::kCPU);
-    std::cout << "Using device: " << (device.is_cuda() ? "CUDA" : "CPU") << std::endl;
-
-    // Create an instance of AbsModel with a 3-layer MLP (Input: 10, Hidden: 50, Output: 2)
-    // Could be multiple layers for example 4-layer MLP {10,20,30,5}
-    NeuralNet model({10, 50, 2}); // This line creates the model instance
-
-    // Move the model to the correct device (CPU or CUDA)
-    model.to(device);
-
-    // Example input tensor (batch of 5 samples, 10 features each)
-    torch::Tensor input = torch::randn({5, 10}).to(device);
-
-    // Run the model on the input
-    torch::Tensor output = model.forward(input);
-
-    std::cout << output << std::endl;
-}
